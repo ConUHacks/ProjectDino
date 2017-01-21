3c0a58f0a59a095d600bb3a226c6d664ee47b401
Matthew Pollack <matt.pck@gmail.com>
2017-01-21 12:44:51 -0500

    Sample endpoint

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ var listener = io.listen(server);
 app.use("/", express.static("public/dashboard/"));
 server.listen(3000, function() {});
 
+app.get("/dan", function(req, res, next) {
+    // req = request;
+    // res = response;
+    // next = next function in chain
+
+    // Return the json datatype 
+    res.json({
+        status: "hi",
+    });
+});
+
+
 /**
  * SERVER SESSIONS
  */
