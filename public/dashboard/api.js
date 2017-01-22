4b4a504a6948d3a31ac2dd29c52ee5a9345bfcc2
Matthew Pollack <matt.pck@gmail.com>
2017-01-22 04:44:32 -0500

    Rock paper scissors for testing

---
+++ public/dashboard/api.js
@@ -0,0 +1,48 @@
+var DinoAPI = function(apiKey) {
+    var socket = io("ws://ec2-54-147-250-149.compute-1.amazonaws.com:3000/");
+    var sessionKey = undefined;
+    var users = [];
+    var callbacks = {};
+
+    this.startHost = function(callback) {
+        socket.emit("identity", {
+            type: "host",
+            apiKey: apiKey,
+        });
+
+        socket.on("identity", function(response) {
+            sessionKey = response.key;
+            callback(response.key);
+        });
+
+        socket.on("ready", function(response) {
+            users[response.id] = {
+                state: {},
+            };
+
+            if (this.onPlayer) {
+                this.onPlayer(response.id);
+            }
+        });
+
+        socket.on("state", function(response) {
+            users[response.id].state[response.state.title] = response.state.state;
+
+            if (callbacks[response.state.title]) {
+                callbacks[response.state.title](response);
+            }
+        });
+    };
+
+    this.getSessionKey = function() {
+        return sessionKey;
+    };
+
+    this.getStateObject = function() {
+        return users;
+    };
+
+    this.onEvent = function(title, callback) {
+        callbacks[title] = callback;
+    };
+};
