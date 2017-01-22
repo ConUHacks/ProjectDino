90cd267761eccc03f86895f03e7912aa9d00ad28
Nicholas Radford <Nradford@bell.net>
2017-01-22 00:11:46 -0500

    Nicks changes

---
+++ Website/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $("#object1").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object2").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object3").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object4").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object5").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object6").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object7").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object8").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object9").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object10").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object11").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object12").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#object13").draggable( {containment: "#divcontainer", scroll: false} );
+
+  $("#wrap1").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap2").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap3").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap4").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap5").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap6").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap7").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap8").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap9").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap10").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap11").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap12").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap13").draggable( {containment: "#divcontainer", scroll: false} );
+
+});
