6e2523d0cec23e0d965986a1b8d60d9ef6751805
Daniel2218 <daniel_lucia22@hotmail.com>
2017-01-21 20:34:01 -0500

    Changes

--- server.js
+++ server.js
@@ -3,18 +3,52 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var listener = io.listen(server);
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", express.static("public/dashboard/"));
 server.listen(3000, function() {});
 
-app.get("/dan", function(req, res, next) {
+var dashboard = require('./app/Dashboard/dashboard.js');
+app.put("/saveController", function(req, res, next) {
     // req = request;
     // res = response;
     // next = next function in chain
 
+    dashboard.saveController(req.body);
     // Return the json datatype
-    res.json({
-        status: "hi",
+    res.status(500).json({error: "There was an error."})
+    res.status(200).json({
+        success: "true"
+    });
+});
+
+app.put("/createOrg", function(req, res, next) {
+    // req = request;
+    // res = response;
+    // next = next function in chain
+
+    dashboard.createOrg(req.body);
+    // Return the json datatype
+    res.status(500).json({error: "There was an error."})
+    res.status(200).json({
+        success: "true"
+    });
+});
+
+app.post("/validate", function(req, res, next) {
+    // req = request;
+    // res = response;
+    // next = next function in chain
+
+    var correct = dashboard.validate(req.body);
+    if (!correct) {
+        res.json(message: "Incorrect Password");
+    }
+    // Return the json datatype
+    res.status(500).json({error: "There was an error."})
+    res.status(200).json({
+        success: "true"
     });
 });
 
