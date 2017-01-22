ef12654bba519b4c238fd6be16b7cfc532c714cb
LucasBullen <bullenlucas@gmail.com>
2017-01-22 06:09:59 -0500

    readd app

---
+++ DinoMobile/DinoMobile/JoinPage.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+
+using DeviceMotion.Plugin;
+using DeviceMotion.Plugin.Abstractions;
+
+namespace DinoMobile
+{
+    public class JoinPage : ContentPage
+    {
+        private API Dino_API;// = DependencyService.Get<API>();
+
+        Entry accessKeyEntry;
+        Label errorLabel;
+        Button enterButton;
+
+        public void GetJoinMessage(object source, object e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                JObject data = JObject.Parse(e.ToString());
+                if (data["error"] != null)
+                {
+                    errorLabel.Text = data["error"].ToString();
+                }
+                else {
+                    //we are good
+                    errorLabel.Text = data["success"].ToString();
+                }
+            });
+        }
+
+        public void GetControllerMessage(object source, object e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                JObject data = JObject.Parse(e.ToString());
+                //get controller
+                //send to Controller Page
+                Navigation.PushAsync(new ControllerPage(data));
+            });
+        }
+
+        public JoinPage()
+        {
+            //Dino_API.init();
+            Dino_API = DependencyService.Get<API>();
+            buildView();
+            Dino_API.OnIdentity += new EventHandler<JObject>(GetJoinMessage);
+            Dino_API.OnController += new EventHandler<JObject>(GetControllerMessage);
+        }
+
+
+        /*Code*/
+
+
+
+        //check if access code is real and if so goes to controller, if not then shows error
+        public void tryAccessCode(string access_code)
+        {
+            if (!String.IsNullOrEmpty(access_code))
+            {
+                Dino_API.init(access_code);
+            }
+            else {
+                errorLabel.Text = "Enter a code";
+            }
+        }
+
+        public bool onIdentity(JObject ret)
+        {
+            if (ret["error"] != null)
+            {
+                errorLabel.Text = ret["error"].ToString();
+            }
+            else {
+                errorLabel.Text = ret["success"].ToString();
+            }
+            return false;
+        }
+
+
+
+
+        /*Visuals*/
+
+
+
+
+        private void buildView()
+        {
+            StackLayout panel = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Orientation = StackOrientation.Vertical,
+                Spacing = 0,
+            };
+            panel.BackgroundColor = Color.White;
+            Title = "Join";
+
+            accessKeyEntry = new Entry
+            {
+                Placeholder = "a1b2c3",
+                HeightRequest = Device.OnPlatform(40, 52, 44)
+            };
+
+            errorLabel = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                HorizontalTextAlignment = TextAlignment.Start,
+                Margin = new Thickness(0, 5, 0, 0),
+                TextColor = Color.Red
+            };
+
+            enterButton = new Button
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                Text = "Join",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Margin = new Thickness(0, 5, 0, 0),
+            };
+
+            enterButton.Clicked += delegate
+            {
+                tryAccessCode(accessKeyEntry.Text);
+            };
+
+            panel.Children.Add(new mPadding(new Label
+            {
+                Text = "Access Code",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Margin = new Thickness(0, 5, 0, 0)
+            }, 10, 50, 10, 0));
+            panel.Children.Add(new mPadding(accessKeyEntry, 10, 50, 10, 0));
+            panel.Children.Add(errorLabel);
+            panel.Children.Add(new mPadding(enterButton, 10, 50, 10, 0));
+
+
+            this.Content = panel;
+        }
+
+        protected override void OnAppearing()
+        {
+            buildView();
+            Dino_API.closeConnection();
+        }
+    }
+    public class mPadding : StackLayout
+    {
+        public mPadding(Xamarin.Forms.View thing, double left, double top, double right, double bottom)
+        {
+            Padding = new Thickness(left, top, right, bottom);
+            Children.Add(thing);
+        }
+    }
+}
---
+++ DinoMobile/iOS/APICaller.iOS.cs
@@ -0,0 +1,128 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using DinoMobile.iOS;
+using System;
+using System.Text;
+using System.Net;
+using System.Collections.Specialized;
+using SystemConfiguration;
+using Newtonsoft.Json.Linq;
+using Quobject.SocketIoClientDotNet.Client;
+
+[assembly: Dependency(typeof(APICaller))]
+
+namespace DinoMobile.iOS
+{
+    public class APICaller : API
+    {
+        private Socket socket;
+        public event EventHandler<JObject> OnIdentity;
+        public event EventHandler<JObject> OnController;
+        public event EventHandler<JObject> OnMessage;
+        //public WebSocket websocket = new WebSocket("ws://ec2-54-147-250-149.compute-1.amazonaws.com:3000");
+        //WebSocket websocket = new WebSocket("ws://localhost:2012/");
+
+        public void closeConnection()
+        {
+            if (socket != null)
+            {
+                socket.Close();
+            }
+        }
+
+        public void log(string a)
+        {
+            Console.WriteLine(a);
+        }
+
+        public void sendMessage(string hole, JObject stuff)
+        {
+            socket.Emit(hole,stuff);
+        }
+
+        public void init(string session_key)
+        {
+            socket = IO.Socket("ws://ec2-54-147-250-149.compute-1.amazonaws.com:3000");
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
+                JObject send = new JObject();
+                send.Add("type", "user");
+                send.Add("sessionKey", session_key);
+                socket.Emit("identity", send);
+            });
+
+            socket.On("identity", (data) =>
+            {
+                if (OnIdentity != null)
+                {
+                    JObject dtd = JObject.Parse(data.ToString());
+                    OnIdentity(this, dtd);
+                }
+            });
+
+            socket.On("template", (data) =>
+            {
+                if (OnController != null)
+                {
+                    JObject dtd = JObject.Parse(data.ToString());
+                    OnController(this, dtd);
+                }
+                //JObject ret = JObject.Parse(data.ToString());
+
+
+                //onIdentity(ret);
+                //if(data)
+               // socket.Disconnect();
+            });
+
+            socket.On("MIDGAMEMESSAGE", (data) =>
+            {
+                if (OnController != null)
+                {
+                    JObject dtd = JObject.Parse(data.ToString());
+                    OnMessage(this, dtd);
+                }
+                //JObject ret = JObject.Parse(data.ToString());
+
+
+                //onIdentity(ret);
+                //if(data)
+                // socket.Disconnect();
+            });
+        }
+
+        //sends a message accross the websocket
+       /* public void sendMessage(string message)
+        {
+           // websocket.Send(message);
+        }
+
+        //sets the function that is called when a message is recieved
+        public void setMessageReceived(Func<string, bool> messageReceiver)
+        {
+            this.messageReceiver = messageReceiver;
+        }
+
+        //sets the function for when a websocket is opened
+        public void setOpened(Action opened)
+        {
+            this.socketOpen = opened;
+        }
+        /***************BAD STUFF***************/
+        /*private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            messageReceiver(e.Message);
+        }*/
+        /*
+        private void websocket_Opened(object sender, EventArgs e)
+        {
+            socketOpen();
+        }
+
+        private void websocket_Closed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Closed Websocket");
+        }*/
+    }
+}
