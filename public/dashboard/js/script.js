dd7010b4fd3e8fec4d2bf3bf96d5175cc8eaf6aa
Nicholas Radford <Nradford@bell.net>
2017-01-22 03:15:52 -0500

    Nicks commit 3:15

--- public/dashboard/js/script.js
+++ public/dashboard/js/script.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
   $("#object8").draggable( {containment: "#divcontainer", scroll: false} );
   $("#object9").draggable( {containment: "#divcontainer", scroll: false} );
   $("#object10").draggable( {containment: "#divcontainer", scroll: false} );
-  $("#object11").draggable( {containment: "#divcontainer", scroll: false} );
-  $("#object12").draggable( {containment: "#divcontainer", scroll: false} );
-  $("#object13").draggable( {containment: "#divcontainer", scroll: false} );
+  // $("#object11").draggable( {containment: "#divcontainer", scroll: false} );
+  // $("#object12").draggable( {containment: "#divcontainer", scroll: false} );
+  // $("#object13").draggable( {containment: "#divcontainer", scroll: false} );
 
   $("#wrap1").draggable( {containment: "#divcontainer", scroll: false} );
   $("#wrap2").draggable( {containment: "#divcontainer", scroll: false} );
@@ -26,5 +26,10 @@ $(document).ready(function(){
   $("#wrap11").draggable( {containment: "#divcontainer", scroll: false} );
   $("#wrap12").draggable( {containment: "#divcontainer", scroll: false} );
   $("#wrap13").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap14").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap15").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap16").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap17").draggable( {containment: "#divcontainer", scroll: false} );
+  $("#wrap18").draggable( {containment: "#divcontainer", scroll: false} );
 
 });
